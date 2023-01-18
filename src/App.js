c22db6fa4fdc9c5962941fe5cae1537d9759205e
Bradley Griffin <bigriff11@gmail.com>
2023-01-18 11:13:30 -0700

    Abstract card and import in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 
 // my api key f9178b25
 const API_URL = "https://www.omdbapi.com?apikey=f9178b25";
 
 const App = () => {
+  let [movies, setMovies] = useState([]);
+
   const searchMovies = async (title) => {
     let response = await fetch(`${API_URL}&s=${title}`);
     let data = await response.json();
 
-    console.log(data.Search);
-  };
-
-  let movie = {
-    Title: "Batman v Superman: Dawn of Justice",
-    Year: "2016",
-    imdbID: "tt2975590",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -38,25 +31,9 @@ const App = () => {
         <img src={SearchIcon} alt="search icon" onClick={() => {}} />
       </div>
       <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie.Year}</p>
-          </div>
-          <div>
-            <img
-              src={
-                movie.Poster === "N/A"
-                  ? "https://via.placeholder.com/400"
-                  : movie.Poster
-              }
-              alt={movie.Title}
-            />
-          </div>
-          <div>
-            <span>{movie.Type}</span>
-            <h3>{movie.Title}</h3>
-          </div>
-        </div>
+        {movies.map((item) => (
+          <MovieCard movie={item} />
+        ))}
       </div>
     </div>
   );
